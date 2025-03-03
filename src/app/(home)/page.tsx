feb49d8aa0d5f1c98775a88aaa8df9a6c91c3368
kartonobinsaleh <kartono.saleh@gmail.com>
2025-03-03 15:49:12 +0700

    chore: update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,29 @@
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+
+      <div className="flex justify-center">
+        <Image
+          src="/logo.svg"
+          alt="My Image"
+          width={128} // Sesuaikan ukuran
+          height={128}
+          className="rounded-lg shadow-lg"
+        />
+      </div>
+
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        You can open{" "}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
-        </Link>{' '}
+        </Link>{" "}
         and see the documentation.
       </p>
     </main>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
+
+export const metadata = {
+  title: "Kodein Kilat",
+  description: "Belajar dan Praktek membuat website secepat kilat",
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
